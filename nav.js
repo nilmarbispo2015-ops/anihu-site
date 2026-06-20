cfafc302166bbfff480e76b182abb26b1f766a8f
Dr Nilmar Bispo <nilmarbispo@anihu.com.br>
2026-06-20 19:49:26 +0000

    feat: pagina drenagem lipofuncional completa

--- nav.js
+++ nav.js
@@ -128,6 +128,7 @@
       <li><a href="/harmonizacao-corporal" data-page="/harmonizacao-corporal">Harmonização Corporal</a></li>
       <li><a href="/endolaser" data-page="/endolaser">Endolaser</a></li>
       <li><a href="/curativos" data-page="/curativos">Curativos</a></li>
+      <li><a href="/drenagem-lipofuncional" data-page="/drenagem-lipofuncional">Drenagem</a></li>
       <li><a href="/blog" data-page="/blog">Blog</a></li>
       <li><a href="https://wa.me/5511966504825?text=Olá!+Gostaria+de+agendar+uma+avaliação." target="_blank" class="nav-cta">Agendar</a></li>
     </ul>
